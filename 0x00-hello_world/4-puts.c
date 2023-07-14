624a156b725dbea98aa382394698b021b2dbb1f4
moonight7 <aniebietmoon@gmail.com>
2023-07-13 18:56:15 -0700

    put.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
  * main - Entry point
  *
  * Return: always 0
- * */
+ **/
 int main(void)
 {
-	puts("\"programming is like building a multiligual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
