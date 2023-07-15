97d1e9adba0e16a56d9d09b708bffccdd0cb2ada
moonight7 <aniebietmoon@gmail.com>
2023-07-15 16:21:09 -0700

    positive is better

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ * this project wil enable operators
+ *  Thit project prints the out put of operators
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* this code samples the operators */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	} else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+
+	} else if (n < 0)
+	{
+		printf("%d is nagative\n", n);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+the aim is to explain rithmetic operators and how to use them, along with logical operators
