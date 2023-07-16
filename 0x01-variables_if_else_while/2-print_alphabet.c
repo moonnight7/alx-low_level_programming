db831c966b9f16fa23ff3e96e76d89744b31be59
moonight7 <aniebietmoon@gmail.com>
2023-07-16 08:42:41 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ *
+ * The essence is to print lowercase alphabets
+*/
+
+int main(void)
+{
+	char a;
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	putchar('\n');
+
+	return (0);
+}
